cd861b7019e0f0a4f4f8872e8fe68366681e9f7b
kirtesh sharma <kirtesh90@gmail.com>
2016-01-05 13:44:34 +0530

    Login and Logout Complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
+
+  # def update
+  #   @user = User.find(params[:id])
+  #   if @user.update_attributes(user_params)
+  #     # Handle a successful update.
+  #   else
+  #     render 'edit'
+  #   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -22,4 +34,5 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
 end

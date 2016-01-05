d1eb2926208d85a3172af65d4ca9ba01f1dee4e9
kirtesh sharma <kirtesh90@gmail.com>
2016-01-05 15:45:26 +0530

    Updation and Autherization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:edit, :update]
+    before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -7,17 +9,6 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  # def edit
-  #   @user = User.find(params[:id])
-  # end
-
-  # def update
-  #   @user = User.find(params[:id])
-  #   if @user.update_attributes(user_params)
-  #     # Handle a successful update.
-  #   else
-  #     render 'edit'
-  #   end
 
   def create
     @user = User.new(user_params)
@@ -29,10 +20,36 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    	redirect_to @user
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-
+	# Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+      	store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+     # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
+
